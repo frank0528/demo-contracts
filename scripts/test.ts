5b67f3881e42f9ca98f966861e55f25d2a2fc626
frank0528 <963688200@qq.com>
2024-06-25 10:03:45 +0800

    init demo contracts

--- scripts/test.ts
+++ scripts/test.ts
@@ -2,13 +2,16 @@ import { ethers } from "hardhat";
 
 async function main() {
   const [deployer] = await ethers.getSigners();
-  console.log(`account: ${deployer.address}`);
 
-  // We get the contract to deploy
   const demo = await ethers.getContractFactory("DemoCoin", deployer);
   const res = await demo.deploy();
+  console.log(`demo address: ${res.address}`);
 
-  console.log('demo address: ' + res.address);
+  const d = demo.attach(res.address);
+
+  console.log(`demo name: ${await d.name()}`);
+  console.log(`demo symbol: ${await d.symbol()}`);
+  console.log(`demo decimals: ${await d.decimals()}`);
 }
 
 main().catch((error) => {
